7aa634d4cbfb40fb92a18ed2ba3c462f5b98ca95
Carls13 <charlesshb98@gmail.com>
2020-05-26 20:30:12 -0400

    Updates

---
+++ precache-manifest.b1d407f93c199798a8ccc2c6ad5ee36e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5146578f41dbcb72e8bf7aaae12f2b07",
+    "url": "/manage/index.html"
+  },
+  {
+    "revision": "38826663537c883b017f",
+    "url": "/manage/static/js/2.0d92a4c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/manage/static/js/2.0d92a4c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf642a9e9cc77c73f165",
+    "url": "/manage/static/js/main.dc7df34d.chunk.js"
+  },
+  {
+    "revision": "46cb8a1e44e34051b973",
+    "url": "/manage/static/js/runtime-main.fbcd9d6a.js"
+  },
+  {
+    "revision": "d022b688c087eb4bc54fa88f43ccb1be",
+    "url": "/manage/static/media/avatar-ali.d022b688.png"
+  },
+  {
+    "revision": "d591dc600c0ee916d50094bdc81d8902",
+    "url": "/manage/static/media/avatar-anisha.d591dc60.png"
+  },
+  {
+    "revision": "a2c4b4309f9936736240c6d7805c7ccf",
+    "url": "/manage/static/media/avatar-richard.a2c4b430.png"
+  },
+  {
+    "revision": "b2507cb2cb85266d8f2051f637b744aa",
+    "url": "/manage/static/media/avatar-shanai.b2507cb2.png"
+  },
+  {
+    "revision": "1a81861b7aaa8c30169d65faf9027218",
+    "url": "/manage/static/media/icon-facebook.1a81861b.svg"
+  },
+  {
+    "revision": "bdd40cf9386a83e11660fab1e8fe9965",
+    "url": "/manage/static/media/icon-hamburger.bdd40cf9.svg"
+  },
+  {
+    "revision": "e0814599d1e638a04578e79a652801c0",
+    "url": "/manage/static/media/icon-instagram.e0814599.svg"
+  },
+  {
+    "revision": "ef15af23a9c6edf1014f6f44b31c8b78",
+    "url": "/manage/static/media/icon-pinterest.ef15af23.svg"
+  },
+  {
+    "revision": "064c31714c77b6580c3e4e8176c435fb",
+    "url": "/manage/static/media/icon-twitter.064c3171.svg"
+  },
+  {
+    "revision": "8e2f566fcefd23c8aacdaa5a83cdaff2",
+    "url": "/manage/static/media/icon-youtube.8e2f566f.svg"
+  },
+  {
+    "revision": "6a53bba8095e0ddefa1791bbd03cf51f",
+    "url": "/manage/static/media/illustration-intro.6a53bba8.svg"
+  },
+  {
+    "revision": "f2cec4b29d7e7f388db937e3f5d9d873",
+    "url": "/manage/static/media/logo.f2cec4b2.svg"
+  }
+]);
